4d0e3a8e92c61511ca8ce9abde77a044b1aa4384
Guilherme Lima <guilhermelimaricart1@gmail.com>
2026-03-05 19:48:01 -0300

    Initial commit

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
+//adicionar os números ao display quando clickar
+
 function AddDisplay(item) {
   document.querySelector("#display").value += item;
 }
 
+//botão "C" para limpar todo o display
+
 function C_clear(item) {
   document.querySelector("#display").value = item;
 }
+
+//botão de Del para apagar o ultimo número do display
 
 function Del_button() {
   const display = document.querySelector('#display');
   display.value = display.value.slice(0, -1)
 }
 
+//botão de resultado 
 
 function res_button() {
   const display = document.querySelector('#display');
